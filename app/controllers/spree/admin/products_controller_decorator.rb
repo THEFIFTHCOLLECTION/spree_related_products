057801ee129d9aaf79beb47437b73e8bd3410322
Dan Williams <dan@331.io>
2014-10-29 22:14:32 -0400

    Add products related by taxon
    The idea is to add the products most likely to be related so it only takes a single click to add them as related products

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,11 @@
 Spree::Admin::ProductsController.class_eval do
   def related
     load_resource
+    p_taxons = @product.taxons
+    @products = {}
+    p_taxons.each do |t|
+      @products[t.name] = t.products
+    end
     @relation_types = Spree::Product.relation_types
   end
 end
